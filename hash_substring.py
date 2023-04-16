2eb741e344b599cd93b360407704951c4dd8d691
aleksandra červinska 221RDB069 <97166400+a13xa@users.noreply.github.com>
2023-04-16 18:22:11 +0000

    Aleksandra Červinska 221RDB069 12.grupa

--- hash_substring.py
+++ hash_substring.py
@@ -3,16 +3,15 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    inp = input().rstrip()
+    inp = input().rstrip().upper()
     if inp == 'F':
         with open('./tests/06','r') as f:
-            pattern=f.readline().rstrip()
-            text=f.readline().rstrip()
-            return pattern.lower(), text.lower()
+            pattern=f.readline().strip()
+            text=f.readline().strip()
     elif inp == 'I':
-        pattern=input().rstrip().lower()
-        text=input().rstrip().lower()
-        return pattern, text
+        pattern=input().strip().lower()
+        text=input().strip().lower()
+        
     else:
         print("wrong inp")
         return None
@@ -22,42 +21,31 @@ def read_input():
     # second line is text in which to look for pattern
     # return both lines in one return
     # this is the sample return, notice the rstrip function
-    
+    return pattern, text
 
 def print_gad(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
+
 def get_gad(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
-    k=31
-    p=10**9 + 9
-    m=len(pattern)
-    n=[1]*(m+1)
-    h=[0]*(len(text)-m+1)
-
-    for i in range(1, m+1):
-        n[i]=(n[i-1]*k)%p
-
-    pattern_hash = sum((ord(pattern[i])*n[m-i])%p for i in range(m))%p
-
-    h[0]=sum(ord(text[i])*n[m-i-1] for i in range(m))%p
-
-    for i in range(len(text)-m+1):
-        if i+m < len(text):
-            h[i+1]=((h[i]-ord(text[i])*n[m-1])*k+ord(text[i+m]))%p
-
+    # implementation of Rabin Karp's algorithm
+    p = 1000000007  
+    q = 10**9+9
+    x = 263
+    n = len(text)
+    m = len(pattern)
+    xp = [pow(x, i, q) for i in range(m)]
+    hp = sum(ord(pattern[i]) * xp[m-1-i] for i in range(m)) % q
+    ht = sum(ord(text[i]) * xp[m-1-i] for i in range(m)) % q
     gad = []
-    for i in range(len(text)-m+1):
-        if h[i] == pattern_hash:
-            j = 0
-            while j < m and text[i+j] == pattern[j]:
-                j += 1
-            if j == m:
-                gad.append(i)
-
-    # and return an iterable variable
+    if ht == hp and text[:m] == pattern:
+        gad.append(0)
+    for i in range(1, n-m+1):
+        ht = (ht - ord(text[i-1]) * xp[m-1]) % q
+        ht = (ht * x + ord(text[i+m-1])) % q
+        if ht == hp and text[i:i+m] == pattern:
+            gad.append(i)
     return gad
-
 
 # this part launches the functions
 if __name__ == '__main__':
