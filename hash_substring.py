d7a3b5fce68ac16b1a21f5156e63f272beb09cf2
aleksandra červinska 221RDB069 <97166400+a13xa@users.noreply.github.com>
2023-04-16 17:55:15 +0000

    Aleksandra Červinska 221RDB069 12.grupa

--- hash_substring.py
+++ hash_substring.py
@@ -1,2 +1,65 @@
 # python3
 ## Aleksandra Červinska 221RDB069 12.grupa
+def read_input():
+    # this function needs to aquire input both from keyboard and file
+    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    inp = input().rstrip()
+    if inp == 'F':
+        with open('./tests/06','r') as f:
+            pattern=f.readline().rstrip()
+            text=f.readline().rstrip()
+            return pattern.lower(), text.lower()
+    elif inp == 'I':
+        pattern=input().rstrip().lower()
+        text=input().rstrip().lower()
+        return pattern, text
+    else:
+        print("wrong inp")
+        return None
+    # after input type choice
+    # read two lines
+    # first line is pattern
+    # second line is text in which to look for pattern
+    # return both lines in one return
+    # this is the sample return, notice the rstrip function
+    
+
+def print_gad(output):
+    # this function should control output, it doesn't need any return
+    print(' '.join(map(str, output)))
+def get_gad(pattern, text):
+    # this function should find the occurances using Rabin Karp alghoritm 
+    k=31
+    p=10**9 + 9
+    m=len(pattern)
+    n=[1]*(m+1)
+    h=[0]*(len(text)-m+1)
+
+    for i in range(1, m+1):
+        n[i]=(n[i-1]*k)%p
+
+    pattern_hash = sum((ord(pattern[i])*n[m-i])%p for i in range(m))%p
+
+    h[0]=sum(ord(text[i])*n[m-i-1] for i in range(m))%p
+
+    for i in range(len(text)-m+1):
+        if i+m < len(text):
+            h[i+1]=((h[i]-ord(text[i])*n[m-1])*k+ord(text[i+m]))%p
+
+    gad = []
+    for i in range(len(text)-m+1):
+        if h[i] == pattern_hash:
+            j = 0
+            while j < m and text[i+j] == pattern[j]:
+                j += 1
+            if j == m:
+                gad.append(i)
+
+    # and return an iterable variable
+    return gad
+
+
+# this part launches the functions
+if __name__ == '__main__':
+    print_gad(get_gad(*read_input()))
+
